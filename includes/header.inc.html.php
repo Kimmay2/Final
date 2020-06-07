e2f10d2055be7612988a63240276ed7d4d3c4087
Kimmay2 <59853084+Kimmay2@users.noreply.github.com>
2020-06-07 13:16:03 -0700

    Add files via upload

---
+++ includes/header.inc.html.php
@@ -0,0 +1,9 @@
+<?php define('BASE_URL', '/cas222/ace/'); ?>
+
+<header class="header">
+
+	<h1>ACE IN THE HOLE MULTISPORT EVENTS</h1> 
+	
+</header>
+	
+
---
+++ includes/mobile.inc.html.php
@@ -0,0 +1,15 @@
+<nav class="mobile">
+<span>Menu</span>
+	<button class="hamburger">&#9776;</button>
+	<button class="cross">&#735;</button>
+
+<div class="menu">
+	<ul>
+		<li><a href="<?php echo BASE_URL; ?>index.php">Home</a></li>
+		<li><a href="<?php echo BASE_URL; ?>Event/events.html.php">Event Info</a></li>
+		<li><a href="<?php echo BASE_URL; ?>Register">Register</a></li>
+		<li><a href="<?php echo BASE_URL; ?>FAQ">FAQ</a></li>
+	</ul>
+
+	</div>
+	</nav>
